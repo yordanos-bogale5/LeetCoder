8506954de9ccca4c54b86016ed1d7e24374defdf
yordanos-bogale5 <145749217+yordanos-bogale5@users.noreply.github.com>
2024-01-04 21:12:55 +0300

    Create 0009. Palindrome Number.py

---
+++ 0009. Palindrome Number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPalindrome(self, x):
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
+
+        reversed_num = 0
+        while x > reversed_num:
+            reversed_num = reversed_num * 10 + x % 10
+            x //= 10
+
+        return x == reversed_num or x == reversed_num // 10
